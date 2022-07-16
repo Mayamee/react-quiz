e87e439f55cd3b4285d9eea729f5c6e004ce03f5
Mew <ponyashcat228@gmail.com>
2022-07-16 14:46:54 +0300

    050 - изменение вопроса

--- src/components/ActiveQuiz/ActiveQuiz.jsx
+++ src/components/ActiveQuiz/ActiveQuiz.jsx
@@ -6,10 +6,12 @@ const ActiveQuiz = (props) => {
     <div className={classes.ActiveQuiz}>
       <p className={classes.Question}>
         <span>
-          <strong>2.</strong>
+          <strong>{props.answerNumber}.</strong>
           &nbsp;{props.question}
         </span>
-        <small>4 из 12</small>
+        <small>
+          {props.answerNumber} из {props.quizLength}
+        </small>
       </p>
       <AnswersList
         answers={props.answers}
--- src/containers/Quiz/Quiz.jsx
+++ src/containers/Quiz/Quiz.jsx
@@ -3,10 +3,12 @@ import classes from "./Quiz.module.scss";
 import ActiveQuiz from "../../components/ActiveQuiz/ActiveQuiz";
 class Quiz extends Component {
   state = {
+    activeQuestion: 0,
     quiz: [
       {
         question: "Какого цвета небо?",
         rightAnswerId: 2,
+        id: 1,
         answers: [
           { text: "Черный", id: 1 },
           { text: "Синий", id: 2 },
@@ -14,10 +16,35 @@ class Quiz extends Component {
           { text: "Желтый", id: 4 },
         ],
       },
+      {
+        question: "Какого цвета луна?",
+        rightAnswerId: 1,
+        id: 2,
+        answers: [
+          { text: "Черный", id: 1 },
+          { text: "Синий", id: 2 },
+          { text: "Зеленый", id: 3 },
+          { text: "Желтый", id: 4 },
+        ],
+      },
+      {
+        question: "В каком году основана Москва?",
+        rightAnswerId: 2,
+        id: 3,
+        answers: [
+          { text: "1789", id: 1 },
+          { text: "1147", id: 2 },
+          { text: "1321", id: 3 },
+          { text: "1789", id: 4 },
+        ],
+      },
     ],
   };
   onAnswerClickHandler = (answerId) => {
     console.log(answerId);
+    this.setState({
+      activeQuestion: this.state.activeQuestion + 1,
+    });
   };
   render() {
     return (
@@ -25,9 +52,11 @@ class Quiz extends Component {
         <div className={classes.QuizWrapper}>
           <h1>Ответьте на все вопросы</h1>
           <ActiveQuiz
-            answers={this.state.quiz[0].answers}
-            question={this.state.quiz[0].question}
+            answers={this.state.quiz[this.state.activeQuestion].answers}
+            question={this.state.quiz[this.state.activeQuestion].question}
             onAnswerClick={this.onAnswerClickHandler}
+            quizLength={this.state.quiz.length}
+            answerNumber={this.state.activeQuestion + 1}
           />
         </div>
       </div>
