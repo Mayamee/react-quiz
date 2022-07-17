0ab6bb727b7dacb62294588e722b070162f84c20
Mew <ponyashcat228@gmail.com>
2022-07-17 11:11:06 +0300

    056 - бургер меню иконка

--- src/hoc/Layout.jsx
+++ src/hoc/Layout.jsx
@@ -1,10 +1,21 @@
 import React, { Component } from "react";
 import classes from "./Layout.module.scss";
+import MenuToggle from "../components/Navigation/MenuToggle/MenuToggle";
 
 class Layout extends Component {
+  state = {
+    isMenuOpen: false,
+  };
+  toggleMenuHandler = () => {
+    this.setState({ isMenuOpen: !this.state.isMenuOpen });
+  };
   render() {
     return (
       <div className={classes.Layout}>
+        <MenuToggle
+          onToggle={this.toggleMenuHandler}
+          isOpen={this.state.isMenuOpen}
+        />
         <main>{this.props.children}</main>
       </div>
     );
