f9eaf12d3f56c3649a5ee15af44f2cbc5233bf79
Mew <ponyashcat228@gmail.com>
2022-07-18 22:33:18 +0300

    074 - Страница Авторизации

--- src/containers/Auth/Auth.jsx
+++ src/containers/Auth/Auth.jsx
@@ -1,9 +1,31 @@
 import React, { Component } from "react";
-
+import classes from "./Auth.module.scss";
+import Button from "../../components/UI/Button/Button";
 class Auth extends Component {
   state = {};
+  loginHandler = () => {};
+  registerHandler() {}
+  submitHandler(event) {
+    event.preventDefault();
+  }
   render() {
-    return <div>Auth</div>;
+    return (
+      <div className={classes.Auth}>
+        <div>
+          <h1>Авторизация</h1>
+          <form className={classes.AuthForm} onSubmit={this.submitHandler}>
+            <input type="text" />
+            <input type="text" />
+            <Button type="success" onClick={this.loginHandler}>
+              Войти
+            </Button>
+            <Button type="primary" onClick={this.registerHandler}>
+              Регистрация
+            </Button>
+          </form>
+        </div>
+      </div>
+    );
   }
 }
 
