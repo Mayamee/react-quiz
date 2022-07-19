c8d9d9d9722f567c3df3d512527b18d90fcffbce
Mew <ponyashcat228@gmail.com>
2022-07-19 19:12:05 +0300

    080 - Создание контролов (мини фреймворк)

--- src/containers/QuizCreator/QuizCreator.jsx
+++ src/containers/QuizCreator/QuizCreator.jsx
@@ -1,27 +1,76 @@
 import React, { Component } from "react";
 import classes from "./QuizCreator.module.scss";
 import Button from "../../components/UI/Button/Button";
+import { createControl } from "../../form/formFramework";
+import Input from "../../components/UI/Input/Input";
+
+function createOptionControl(number) {
+  return createControl(
+    {
+      label: "Введите вопрос №" + number,
+      errorMessage: "Вопрос не может быть пустым",
+      id: number,
+    },
+    { required: true }
+  );
+}
+
+function createFormControls() {
+  return {
+    question: createControl(
+      {
+        label: "Введите вопрос",
+        errorMessage: "Вопрос не может быть пустым",
+      },
+      { required: true }
+    ),
+    option1: createOptionControl(1),
+    option2: createOptionControl(2),
+    option3: createOptionControl(3),
+    option4: createOptionControl(4),
+  };
+}
 
 class QuizCreator extends Component {
-  state = {};
+  state = {
+    quiz: [],
+    formControls: createFormControls(),
+  };
 
   submitHandler = (event) => {
     event.preventDefault();
   };
   addQuestionHandler = () => {};
   createQuizHandler = () => {};
+  onChangeHandler(value, controlName) {}
+  renderInputs() {
+    return Object.keys(this.state.formControls).map((controlName, index) => {
+      const control = this.state.formControls[controlName];
+      return (
+        <React.Fragment key={controlName + index}>
+          <Input
+            label={control.label}
+            value={control.value}
+            errorMessage={control.errorMessage}
+            shouldValidate={!!control.validation}
+            valid={control.valid}
+            touched={control.touched}
+            onChange={(event) =>
+              this.onChangeHandler(event.target.value, controlName)
+            }
+          />
+          {index === 0 ? <hr /> : null}
+        </React.Fragment>
+      );
+    });
+  }
   render() {
     return (
       <div className={classes.QuizCreator}>
         <div>
           <h1>Создание теста</h1>
           <form onSubmit={this.submitHandler}>
-            <input type="text" />
-            <hr />
-            <input type="text" />
-            <input type="text" />
-            <input type="text" />
-            <input type="text" />
+            {this.renderInputs()}
             <select></select>
             <Button btnType="primary" onClick={this.addQuestionHandler}>
               Добавить вопрос
