0fbe63913714bfa39c8390f711e0022952f78468
Mew <ponyashcat228@gmail.com>
2022-07-19 18:47:45 +0300

    079 - Создание тестов

--- src/containers/QuizCreator/QuizCreator.jsx
+++ src/containers/QuizCreator/QuizCreator.jsx
@@ -1,10 +1,38 @@
 import React, { Component } from "react";
 import classes from "./QuizCreator.module.scss";
+import Button from "../../components/UI/Button/Button";
 
 class QuizCreator extends Component {
   state = {};
+
+  submitHandler = (event) => {
+    event.preventDefault();
+  };
+  addQuestionHandler = () => {};
+  createQuizHandler = () => {};
   render() {
-    return <div>QuizCreator</div>;
+    return (
+      <div className={classes.QuizCreator}>
+        <div>
+          <h1>Создание теста</h1>
+          <form onSubmit={this.submitHandler}>
+            <input type="text" />
+            <hr />
+            <input type="text" />
+            <input type="text" />
+            <input type="text" />
+            <input type="text" />
+            <select></select>
+            <Button btnType="primary" onClick={this.addQuestionHandler}>
+              Добавить вопрос
+            </Button>
+            <Button btnType="success" onClick={this.createQuizHandler}>
+              Создать тест
+            </Button>
+          </form>
+        </div>
+      </div>
+    );
   }
 }
 
