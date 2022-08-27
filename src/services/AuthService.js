7eb24f62786fedb3dd6d7386c73d8eecb259af71
Mew <54446552+Mayamee@users.noreply.github.com>
2022-08-27 16:21:17 +0300

    fix $ from axiosAuth

--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -1,19 +1,19 @@
-import { axiosAuth } from "../http/axiosRequests";
+import { $axiosAuth } from "../http/axiosRequests";
 
 export default class AuthService {
   static login(email, password) {
-    return axiosAuth.post("/registration", {
+    return $axiosAuth.post("/registration", {
       email,
       password,
     });
   }
   static register(email, password) {
-    return axiosAuth.post("/registration", {
+    return $axiosAuth.post("/registration", {
       email,
       password,
     });
   }
   static logout() {
-    return axiosAuth.get("/logout");
+    return $axiosAuth.get("/logout");
   }
 }
