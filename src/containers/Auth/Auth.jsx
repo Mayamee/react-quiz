137d9e6b246b083a6420cef156e4649af5b9074d
Mayamee <ponyashcat228@gmail.com>
2022-08-30 18:05:41 +0300

    рефактор auth на функциональный компонент

--- src/containers/Auth/Auth.jsx
+++ src/containers/Auth/Auth.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import { useState } from "react";
 import classes from "./Auth.module.scss";
 import Button from "../../components/UI/Button/Button";
 import Input from "../../components/UI/Input/Input";
@@ -7,11 +7,7 @@ import { email, maxLength, minLength } from "../../validation/RuleCreator";
 import { validateFormFields } from "../../helpers/valid";
 import { createValidationInputField } from "../../helpers/formInputCreator";
 import { connect } from "react-redux";
-import {
-  authLogin,
-  authLogout,
-  authRegister,
-} from "../../store/actions/authorization";
+import { authLogin, authRegister } from "../../store/actions/authorization";
 
 const initForm = () => [
   createValidationInputField("Email", email(), "email"),
@@ -22,93 +18,79 @@ const initForm = () => [
   ),
 ];
 
-class Auth extends Component {
-  state = {
-    isFormValid: false,
-    formFields: initForm(),
-  };
-  loginHandler() {
-    this.props.login(
-      this.state.formFields[0].value,
-      this.state.formFields[1].value
-    );
-    this.setState({
-      formFields: initForm(),
-      isFormValid: false,
-    });
-  }
-  registerHandler() {
-    this.props.register(
-      this.state.formFields[0].value,
-      this.state.formFields[1].value
-    );
-  }
+const Auth = ({ login, register, isAuth, msg }) => {
+  const [isFormValid, setFormValid] = useState(false);
+  const [formFields, setFormFields] = useState(initForm());
 
-  onChangeHandler(id, value, isValid) {
-    const formFields = [...this.state.formFields];
-    const field = formFields[id];
+  const loginHandler = () => {
+    login(formFields[0].value, formFields[1].value);
+    setFormValid(false);
+    setFormFields(initForm());
+  };
+  const registerHandler = () => {
+    register(formFields[0].value, formFields[1].value);
+    setFormValid(false);
+    setFormFields(initForm());
+  };
+
+  const onChangeHandler = (id, value, isValid) => {
+    const formFieldsCopy = [...formFields];
+    const field = formFieldsCopy[id];
     field.value = value;
     field.isValid = isValid;
-    formFields[id] = field;
-    const isFormValid = validateFormFields(formFields);
-    this.setState({
-      formFields,
-      isFormValid,
-    });
-  }
-  renderInputs() {
-    return this.state.formFields.map((field, index) => (
+    formFieldsCopy[id] = field;
+    const isFormValid = validateFormFields(formFieldsCopy);
+    setFormValid(isFormValid);
+    setFormFields(formFieldsCopy);
+  };
+  const renderInputs = () =>
+    formFields.map((field, index) => (
       <Validation rules={field.validationRules} key={`${field.label}-${index}`}>
         <Input
           type={field.type}
           value={field.value}
           label={field.label}
-          onChange={this.onChangeHandler.bind(this, index)}
+          onChange={onChangeHandler.bind(this, index)}
         />
       </Validation>
     ));
-  }
 
-  render() {
-    return (
-      <div className={classes.Auth}>
-        <div>
-          <h1>Авторизация</h1>
-          <div className="message">{this.props.msg}</div>
-          <form
-            className={classes.AuthForm}
-            onSubmit={(e) => e.preventDefault()}
+  return (
+    <div className={classes.Auth}>
+      <div>
+        <h1>Авторизация</h1>
+        <div className="message">{msg}</div>
+        <form className={classes.AuthForm} onSubmit={(e) => e.preventDefault()}>
+          <div className="inputsContainer">{renderInputs()}</div>
+          <Button
+            btnType="success"
+            onClick={(e) => {
+              e.preventDefault();
+              loginHandler();
+            }}
+            disabled={!isFormValid}
           >
-            <div className="inputsContainer">{this.renderInputs()}</div>
-            <Button
-              btnType="success"
-              onClick={(e) => {
-                e.preventDefault();
-                this.loginHandler();
-              }}
-              disabled={!this.state.isFormValid}
-            >
-              Войти
-            </Button>
-            <Button
-              btnType="primary"
-              onClick={(e) => {
-                e.preventDefault();
-                this.registerHandler();
-              }}
-              disabled={!this.state.isFormValid}
-            >
-              Регистрация
-            </Button>
-          </form>
-        </div>
+            Войти
+          </Button>
+          <Button
+            btnType="primary"
+            onClick={(e) => {
+              e.preventDefault();
+              registerHandler();
+            }}
+            disabled={!isFormValid}
+          >
+            Регистрация
+          </Button>
+        </form>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 const mapStateToProps = (state) => ({
   msg: state.auth.msg,
+  isAuth: state.auth.isAuthentificated,
 });
 
 const mapDispatchToProps = (dispatch) => ({
