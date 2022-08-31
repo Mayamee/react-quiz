e122626f612b9d06e746bea7c65676426d26f26c
Mayamee <ponyashcat228@gmail.com>
2022-08-31 16:20:05 +0300

    правка layout

--- src/hoc/Layout/Layout.jsx
+++ src/hoc/Layout/Layout.jsx
@@ -1,11 +1,11 @@
 import NavBar from "../../components/NavBar/NavBar";
 import classes from "./Layout.module.scss";
 
-const Layout = ({ isAuth }) => {
+const Layout = ({ isAuth, children }) => {
   return (
     <div className={classes.Layout}>
       <NavBar isAuth={isAuth} />
-      <main>{this.props.children}</main>
+      <main>{children}</main>
     </div>
   );
 };
