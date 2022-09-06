1924d8bcc9717f3e277c0deecd907f096c359998
Mew <54446552+Mayamee@users.noreply.github.com>
2022-09-06 22:53:10 +0300

    Quiz list from class component to FC

--- src/containers/QuizList/QuizList.jsx
+++ src/containers/QuizList/QuizList.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import { useEffect } from "react";
 import { connect } from "react-redux";
 import classes from "./QuizList.module.scss";
 import { NavLink } from "react-router-dom";
@@ -10,46 +10,50 @@ import {
   fetchQuizes,
 } from "../../store/actions/quizActions";
 
-class QuizList extends Component {
-  renderQuizesList() {
-    const quizes = this.props.isAuth ? this.props.quizes : this.props.cached;
+const QuizList = ({
+  quizes: Pquizes,
+  cached,
+  isLoading,
+  userName,
+  isAuth,
+  fetchQuizes,
+  stopLoad,
+  clearQuizes,
+}) => {
+  const renderQuizesList = () => {
+    const quizes = isAuth ? Pquizes : cached;
     return quizes.map((quiz) => (
       <li key={quiz.id}>
         <NavLink to={`/quiz/${quiz.id}`}>{quiz.title}</NavLink>
       </li>
     ));
-  }
-
-  componentDidMount() {
-    if (this.props.isAuth) {
-      this.props.fetchQuizes();
+  };
+  useEffect(() => {
+    if (isAuth) {
+      fetchQuizes();
     } else {
-      this.props.stopLoad();
+      stopLoad();
     }
-  }
-  componentWillUnmount() {
-    this.props.clearQuizes();
-  }
-  render() {
-    return (
-      <div className={classes.QuizList}>
-        <div>
-          <h1>Список тестов</h1>
-          {this.props.isAuth && <h2>Привет: {this.props.userName}</h2>}
-          {!this.props.isAuth && <h2>Привет: Guest</h2>}
-          {this.props.isLoading ? (
-            <Loader />
-          ) : this.props.quizes.length === 0 &&
-            this.props.cached.length === 0 ? (
-            <Nodata iconColor="#fff" isShowButton={false} />
-          ) : (
-            <ul>{this.renderQuizesList()}</ul>
-          )}
-        </div>
+    return clearQuizes;
+  }, []);
+
+  return (
+    <div className={classes.QuizList}>
+      <div>
+        <h1>Список тестов</h1>
+        {isAuth && <h2>Привет: {userName}</h2>}
+        {!isAuth && <h2>Привет: Guest</h2>}
+        {isLoading ? (
+          <Loader />
+        ) : Pquizes.length === 0 && cached.length === 0 ? (
+          <Nodata iconColor="#fff" isShowButton={false} />
+        ) : (
+          <ul>{renderQuizesList()}</ul>
+        )}
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 const mapStateToProps = (state) => {
   return {
