4c5476be9eb0c5c8ba0a0097c20e148e0bf5db2f
Mayamee <ponyashcat228@gmail.com>
2022-10-16 16:10:24 +0300

    fix vars

--- src/vars.js
+++ src/vars.js
@@ -1,3 +1,3 @@
-export const SERVER_URL = 'http://localhost:8080'
+export const SERVER_URL = process.env.SERVER_URL || 'http://localhost:8080'
+export const APP_URL = process.env.APP_URL || 'http://localhost:8081'
 export const API_URL = `${SERVER_URL}/api`
-export const APP_URL = 'http://localhost:8081'
